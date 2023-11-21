e3269abe35040d510b4376b69692e6f7faaa2a60
k <Kylel@email.arizona.edu>
2023-11-21 00:00:51 -0700

    partial function for multiple robots

--- main.c
+++ main.c
@@ -58,68 +58,52 @@ double distance(RobotState s1, RobotState s2);
 
 void PrintRobot(char *name, double d, double x0, double y0, double xf, double yf, double t){
    printf("Robot %s has traveled %.4lf meters, starting at (%.4lf, %.4lf)", name, d, y0, x0);
-   printf("\n ending at (%.4lf, %.4lf) at time %.4lf seconds.", xf, yf, t);
+   printf("\n ending at (%.4lf, %.4lf) at time %.4lf seconds.\n", xf, yf, t);
    return;
 }
 
 int main(void){
    
-   double T, x, y, theta, v[50], w[50], d[50], x_next, y_next, theta_next, x10, y10, theta10;
-   int k=0, robotNum =0,r;
-   char name[50][50], keyWord[6], buf[50];
+   double T, x, y, theta, v[50], w[50], d[50], x_next, y_next, theta_next, x10, y10, theta10, b1, b2;
+   int k=0, robotNum =0,first_line;
+   char name[50][50], buf1[80], buf2[80], *end1, *end2;
    RobotControl u;
    RobotState p, s;
    
-   scanf("%s %s", keyWord, buf);
-   
-   if(strcmp(keyWord, "robot")==0){
+   while(scanf("%s %s", buf1, buf2) == 2){
+   b1 = strtod(buf1, &end1);
+   b2 = strtod(buf2, &end2);
+   if(b1==-1||b2==-1){break;}
+   if(strcmp(buf1, "robot")==0){//
+      if(robotNum>0){PrintRobot(name[robotNum-1], d[k], x, y, s.x, s.y, T*k);}
       robotNum++;
-      strcpy(&name[robotNum-1][0], buf);
+      strcpy(&name[robotNum-1][0], buf2);
+      first_line = 1;
    }
    
+   if(first_line == 1){
    scanf("%lf %lf %lf %lf", &T, &x, &y, &theta);
-   x_next = x;
-   y_next = y;
-   theta_next = theta;
+   s.x = x;
+   s.y = y;
+   s.theta = theta;
    d[0] = 0;
+   k=0;
+   }
    
-   for(k=1; scanf("%lf %lf", &v[k], &w[k]) == 2; k++){//debug
-      if(v[k] == -1 || w[k] == -1){break;}
-      u.v = v[k]; u.w = w[k];
+   if(first_line == 0){
+      v[k] = b1;
+      w[k] = b2;
+      
+      u.v = v[k]; 
+      u.w = w[k];
       p = s;
       s = robot(T, s, u);
       d[k] = d[k-1] + distance(p, s);
-      if(k==5){
-         x = s.x;
-         y = s.y;
-         theta = s.theta;
-      }
-      if(k==10){
-         x10 = s.x;
-         y10 = s.y;
-         theta10 = s.theta;
-      }
-      //printf("-   %d   -", outs);//-debug only
-   }k--;
-   //todo replace printf's with PrintRobot's
-   if(k==5 || k==10){
-      printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(k), s.x, s.y, s.theta);
    }
-   else{
-      if(k<5){
-         printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(k), s.x, s.y, s.theta);
-         PrintRobot(&name[robotNum-1][0], d[k], x, y, s.x, s.y, T*k);
-      }
-      else{
-         if(k < 10){
-            printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(5), x, y, theta);
-         }
-         if(k > 10){
-            printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(5), x, y, theta);
-            printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(10), x10, y10, theta10);
-         }
-      printf("%.4lf: %.4lf, %.4lf, %.4lf\n", T*(k), s.x, s.y, s.theta);
-      }
+   
+   first_line=0;
+   k++;
+   
    }
    return 0;
 }
